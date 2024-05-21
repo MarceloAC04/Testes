bf41f7e92f9ac789daeffa0f3b3f77c1441e863a
Marcelo <marcelocardoso020304@gmail.com>
2024-05-21 10:51:42 -0300

    implementados testes

--- senaify/cypress/e2e/search.cy.js
+++ senaify/cypress/e2e/search.cy.js
@@ -1,5 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Teste da tela de pesquisa', () => {
+  before(() => {
+    cy.visit('/')
+  });
+
+  it('Redirecionar a minha tela para a tela de busca', () => {
+    cy.get("[href='/Search']").click()
+
+    cy.scrollTo("top")
+  });
+
+  it('Procurar uma música', () => {
+    cy.get("[data-testid='campoBusca']").type("Roots Bloody Roots")
+    cy.get("[aria-label='music-item']").should("have.length.greaterThan", 0)
+    
+    // cy.get("[aria-label='music-item']").filter(':contains("Roots Bloody Roots")',   { matchCase: true }).click()
+    cy.scrollTo("top")
+  });
+
+  it('Clicou no botao de curtir', async () => {
+    cy.wait(1500)
+
+    cy.get("[aria-label='music-item']").filter(":contains('Roots Bloody Roots')").then(async (item) => {
+      cy.wrap(item).find("[data-testid='icon-button']").click()
+    });
+
+    
+  });
 })
